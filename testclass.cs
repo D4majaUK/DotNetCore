b689a966ddefdb5476d00fb8de9e37b14079bbb6
Anthony Barrett <Anthony.Barrett@ratesetter.com>
2019-04-16 16:32:30 +0100

    Separating the test classes

--- testclass.cs
+++ testclass.cs
@@ -1,7 +1,7 @@
 using Xunit;
 using fullDotNet.Pages;
 
-public class testclass
+public class testclass1
 {
     [Fact]
     public void PassingValTest()
@@ -12,19 +12,27 @@ public class testclass
         Assert.Equal("This is a function code-behind", resulta);
         Assert.Equal("This is a response that I do as default", resultb);
     }
+}
 
+public class testclass2
+{
     [Fact]
     public void PassingCalcTest()
     {
         double result = myMethods.getCalc(17);
         Assert.Equal(402.9, result);
     }
-
+}
+public class testclass3
+{
     [Fact]
     public void PassingAddTest()
     {
         Assert.Equal(4, fullDotNet.Program.Add(2,2));
     }
+}
+public class testclass4
+{
 
     [Theory]
     [InlineData(3)]
